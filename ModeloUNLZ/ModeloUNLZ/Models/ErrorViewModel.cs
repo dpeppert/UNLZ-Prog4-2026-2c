0f51460957a447aba71184cc88d28f8f2e066b0d
ext_dpeppert <ext_dpeppert@BMN-NG74>
2026-08-10 19:00:29 -0300

    Primer commit

---
+++ ModeloUNLZ/ModeloUNLZ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModeloUNLZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
