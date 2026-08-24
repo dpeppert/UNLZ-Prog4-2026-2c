04bf47a32837c94f63414a336f47a378c4b7417c
ext_dpeppert <ext_dpeppert@BMN-NG74>
2026-08-24 19:34:28 -0300

    Agrego comentario de ejemplo

--- ModeloUNLZ/ModeloUNLZ/Models/ErrorViewModel.cs
+++ ModeloUNLZ/ModeloUNLZ/Models/ErrorViewModel.cs
@@ -5,5 +5,9 @@ namespace ModeloUNLZ.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+
+
+        //comentario
     }
 }
